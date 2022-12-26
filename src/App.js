4817d81b54270e52d6e1b1661a1f8a8c4bc7574b
milkbreadzee <riaelisabeths@gmail.com>
2022-12-26 21:48:03 +0530

    page 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import Sponsers from './components/Sponsers/Sponsers';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <Sponsers />
     </div>
   );
 }
--- src/components/Sponsers/Sponsers.js
+++ src/components/Sponsers/Sponsers.js
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import './Sponsers.css'
 const Sponsers = () => {
   return (
     <div>
-        
+        <div className="sponsers">
+            <ul>
+                <li>
+                    <p className="head">
+                        ✧Capterra
+                    </p>
+                    
+                    <p className='cap'>                        Best Ease of Use
+</p>
+                </li>
+                <li>
+                    <p className="head">
+                        ✧TrustPiolet
+                    </p>
+                    
+                    <p className='cap'>                        4.7 average rating
+</p>
+                </li>
+                <li>
+                    <p className="head">
+                        ☆Crowd
+                    </p>
+                    
+                    <p className='cap'>                        Easiest Setup Summer 2021
+</p>
+                </li>
+                <li>
+                    <p className="head">
+                        ☆Crowd
+                    </p>
+                    
+                    <p className='cap'>                        Best Usability Summer 2021
+</p>
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
